fcf80679621880d7473c2d837dcc7610d78b841b
sujeet-gandhi <sujeetgandhi014@gmail.com>
2023-02-16 11:22:58 -0500

    name of the team and btn to bring new activity

---
+++ app/src/main/java/com/neu/numad23sp_team_34/AtYourService.java
@@ -0,0 +1,14 @@
+package com.neu.numad23sp_team_34;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtYourService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+}
--- app/src/main/java/com/neu/numad23sp_team_34/MainActivity.java
+++ app/src/main/java/com/neu/numad23sp_team_34/MainActivity.java
@@ -2,13 +2,28 @@ package com.neu.numad23sp_team_34;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button atYourServiceBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        atYourServiceBtn = (Button) findViewById(R.id.atYourService);
+
+        atYourServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AtYourService.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
