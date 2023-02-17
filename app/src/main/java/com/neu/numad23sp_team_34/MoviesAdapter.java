944e1d6c271c73779315e42db2c8883ab059ea5e
manjotbedi <manjotmb20@gmail.com>
2023-02-17 14:28:46 -0500

    Adding changes for if image not there then rollback to default image

--- app/src/main/java/com/neu/numad23sp_team_34/MoviesAdapter.java
+++ app/src/main/java/com/neu/numad23sp_team_34/MoviesAdapter.java
@@ -1,5 +1,8 @@
 package com.neu.numad23sp_team_34;
 
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,8 @@ import java.util.List;
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>{
 
     private List<Movie> movies;
+    private Context context;
+
 
 
     public MoviesAdapter(List<Movie> movies) {
@@ -26,6 +31,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
     @NonNull
     @Override
     public MoviesAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view, parent, false);
         return new MovieViewHolder(view);
     }
@@ -35,7 +41,38 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         Movie movie = movies.get(position);
         holder.title.setText(movie.getTitle());
         holder.year.setText(movie.getYear());
-        Picasso.get().load(movie.getImageUrl()).into(holder.poster);
+        if (!TextUtils.isEmpty(movie.getImageUrl())) {
+            Picasso.get().load(movie.getImageUrl()).into(holder.poster, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+
+
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    //do smth when there is picture loading error
+                    holder.poster.setImageResource(R.drawable.ic_launcher_background);
+
+                }
+            });
+        } else {
+            holder.poster.setImageResource(R.drawable.ic_launcher_background);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailViewActivity.class);
+                intent.putExtra("title", movie.getTitle());
+                intent.putExtra("year", movie.getYear());
+                intent.putExtra("imageUrl", movie.getImageUrl());
+                intent.putExtra("imdbId", movie.getImdbId());
+                context.startActivity(intent);
+            }
+        });
+
+
 
     }
 
