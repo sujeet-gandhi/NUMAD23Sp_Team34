37a848ecfd3f113bcb163083d389b15849c7a1bc
manjotbedi <manjotmb20@gmail.com>
2023-03-02 11:17:51 -0500

    Adding Firebase plugins and json files along with some java classes for schema

---
+++ app/src/main/java/com/neu/numad23sp_team_34/Message.java
@@ -0,0 +1,34 @@
+package com.neu.numad23sp_team_34;
+
+public class Message {
+    private String senderUsername;
+    private String receiverUsername;
+    private String stickerId;
+    private long timestamp;
+
+    public Message() {}
+
+    public Message(String senderUsername, String receiverUsername, String stickerId, long timestamp) {
+        this.senderUsername = senderUsername;
+        this.receiverUsername = receiverUsername;
+        this.stickerId = stickerId;
+        this.timestamp = timestamp;
+    }
+
+    public String getSenderUsername() {
+        return senderUsername;
+    }
+
+    public String getReceiverUsername() {
+        return receiverUsername;
+    }
+
+    public String getStickerId() {
+        return stickerId;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
+
---
+++ app/src/main/java/com/neu/numad23sp_team_34/User.java
@@ -0,0 +1,28 @@
+package com.neu.numad23sp_team_34;
+
+public class User {
+    private String username;
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(String username, String name, String email) {
+        this.username = username;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
