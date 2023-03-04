60a5f2c89a8a67be6eb8061b7ccfbf221afa3374
Gagana Ananda <gagana.131214@gmail.com>
2023-03-03 19:44:03 -0500

    HistoryActivity

---
+++ app/src/main/java/com/neu/numad23sp_team_34/sticktoem/HistoryActivity.java
@@ -0,0 +1,14 @@
+package com.neu.numad23sp_team_34.sticktoem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
