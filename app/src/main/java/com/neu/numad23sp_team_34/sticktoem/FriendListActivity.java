b35afba663a18054733c6ea7fe7a524fb6b0d9d5
Gagana Ananda <gagana.131214@gmail.com>
2023-03-04 17:17:13 -0500

    Messages Count

--- app/src/main/java/com/neu/numad23sp_team_34/sticktoem/FriendListActivity.java
+++ app/src/main/java/com/neu/numad23sp_team_34/sticktoem/FriendListActivity.java
@@ -1,13 +1,17 @@
 package com.neu.numad23sp_team_34.sticktoem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -15,11 +19,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.neu.numad23sp_team_34.R;
 import com.neu.numad23sp_team_34.sticktoem.adapters.FriendListAdapter;
+import com.neu.numad23sp_team_34.sticktoem.models.Message;
 import com.neu.numad23sp_team_34.sticktoem.models.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FriendListActivity extends AppCompatActivity {
@@ -36,17 +43,32 @@ public class FriendListActivity extends AppCompatActivity {
 
     private String currentUsername;
 
+    Button history;
+
+    ChatActivity chatActivity;
+
+    HashMap<String, Integer> stickerCounts = new HashMap<>();
+    HashMap<String, Integer> recievedCount = new HashMap<>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+        history = (Button) findViewById(R.id.historyButton);
 
         friendListRecyclerView = findViewById(R.id.friendList);
 
+
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         currentUsername = prefs.getString("username", "");
+//        SharedPreferences prefs1 = PreferenceManager.getDefaultSharedPreferences(this);
+//        currentUsername = prefs.getString("username", "");
 
         friendList = new ArrayList<>();
+
 
         friendListAdapter = new FriendListAdapter(this, friendList);
         friendListRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
@@ -71,7 +93,7 @@ public class FriendListActivity extends AppCompatActivity {
                     @Override
                     public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                         Log.v(TAG, "onChildChanged: " + dataSnapshot.getValue().toString());
-                        Toast.makeText( FriendListActivity.this, "onChildChanged: dataSnapshot = " + dataSnapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(FriendListActivity.this, "onChildChanged: dataSnapshot = " + dataSnapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -92,5 +114,66 @@ public class FriendListActivity extends AppCompatActivity {
                     }
                 }
         );
+        fetchHistory();
+
     }
+    private void fetchHistory(){
+
+        mDatabase.child("messages").addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Message> messages = new ArrayList<>();
+                List<Message> receivedMes = new ArrayList<>();
+                for(DataSnapshot ds: dataSnapshot.getChildren() ){
+                    Message message = ds.getValue(Message.class);
+                    if (message.getSenderUsername().equals(currentUsername)) {
+                        messages.add(message);
+                    }
+                    if(message.getReceiverUsername().equals(currentUsername)){
+                        receivedMes.add(message);
+                    }
+                }
+                List<String> stickerIds = new ArrayList<>();
+                stickerIds.add("2131165428");
+                stickerIds.add("2131165429");
+                stickerIds.add("2131165430");
+                stickerIds.add("2131165431");
+                for (String stickerId: stickerIds) {
+                    int num = countStickers(messages, stickerId);
+                    stickerCounts.put(stickerId, num);
+                }
+                Log.d(TAG,"Sticker Counts" + stickerCounts);
+                for(String receivedStickerId: stickerIds) {
+                    int receivedStickerCount = countStickers(receivedMes,receivedStickerId);
+                    recievedCount.put(receivedStickerId,receivedStickerCount);
+                }
+                Log.d(TAG,"received Counts " + recievedCount);
+                history.setOnClickListener(view -> {
+                    Intent intent = new Intent(FriendListActivity.this,HistoryActivity.class);
+                    intent.putExtra("stickerCount", stickerCounts.toString());
+                    intent.putExtra("receivedCount", recievedCount.toString());
+                    startActivity(intent);
+                });
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        }
+        );
+        // count and update the counter map
+
+    }
+    public int countStickers(List<Message> messages, String stickerId) {
+        int count = 0;
+        for (Message message : messages) {
+            if (message.getStickerId() != null && message.getStickerId().equals(stickerId)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
