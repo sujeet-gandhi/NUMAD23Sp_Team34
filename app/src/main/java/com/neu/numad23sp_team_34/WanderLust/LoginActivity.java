2699c49f53a1e0a2583ba0bf40688924c59b0771
Gagana Ananda <gagana.131214@gmail.com>
2023-03-28 18:32:51 -0400

    RegistrationAuthentication

--- app/src/main/java/com/neu/numad23sp_team_34/WanderLust/LoginActivity.java
+++ app/src/main/java/com/neu/numad23sp_team_34/WanderLust/LoginActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.neu.numad23sp_team_34.R;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button login;
 
     private Button newAccount;
+
+    EditText email;
+
+    EditText password;
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +36,40 @@ public class LoginActivity extends AppCompatActivity {
 
         login = (Button) findViewById(R.id.login);
         newAccount = (Button) findViewById(R.id.createAccount);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        password = (EditText) findViewById(R.id.editTextTextPassword);
+
+        String email_id = email.getText().toString();
+        String pwd = password.getText().toString();
+
+
+            firebaseDatabase.getReference().child("WanderLustUser").child(String.valueOf(0)).child(email_id).setValue(pwd);
+
+            login.setOnClickListener(view -> {
+                if(TextUtils.isEmpty(email_id)){
+                    Toast.makeText(LoginActivity.this,"Please enter email-id!",Toast.LENGTH_SHORT).show();
+                    email.setError("This field cannot be empty");
+                    email.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(email_id).matches()){
+                    Toast.makeText(LoginActivity.this,"Please enter email-id in correct format!",Toast.LENGTH_SHORT).show();
+                    email.setError("Not correct format");
+                    email.requestFocus();
+                }
+                if(pwd.isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Please enter the password!",Toast.LENGTH_SHORT).show();
+                    password.setError("This field cannot be empty");
+                    password.requestFocus();
+                }
+
+            });
+
 
         newAccount.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this,NewAccountActivity.class);
             startActivity(intent);
         });
+
+
     }
+
 }
--- app/src/main/java/com/neu/numad23sp_team_34/WanderLust/NewAccountActivity.java
+++ app/src/main/java/com/neu/numad23sp_team_34/WanderLust/NewAccountActivity.java
@@ -1,16 +1,114 @@
 package com.neu.numad23sp_team_34.WanderLust;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 import com.neu.numad23sp_team_34.R;
 
 public class NewAccountActivity extends AppCompatActivity {
 
+    EditText edtusername;
+    EditText edtemail;
+    EditText edtpassword;
+    EditText edtconfirmPassword;
+    Button signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+
+        edtusername = (EditText) findViewById(R.id.Username);
+        edtemail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        edtpassword = (EditText) findViewById(R.id.editTextTextPassword);
+        edtconfirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        signup = (Button) findViewById(R.id.Signup);
+
+
+
+
+
+        signup.setOnClickListener(view -> {
+
+            String username = edtusername.getText().toString();
+            String email = edtemail.getText().toString();
+            String password = edtpassword.getText().toString();
+            String confirmPassword = edtconfirmPassword.getText().toString();
+
+            if(username.isEmpty()){
+                edtusername.setError("This field cannot be empty");
+                edtusername.requestFocus();
+                Toast.makeText(NewAccountActivity.this,"Please enter a user " +
+                        "name",Toast.LENGTH_SHORT).show();
+            }else if(TextUtils.isEmpty(email)){
+                Toast.makeText(NewAccountActivity.this,"Please enter email-id" +
+                        "!",Toast.LENGTH_SHORT).show();
+                edtemail.setError("This field cannot be empty");
+                edtemail.requestFocus();
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(edtemail.getText().toString()).matches()){
+                Toast.makeText(NewAccountActivity.this,"Please " +
+                        "enter email-id in correct format!",Toast.LENGTH_SHORT).show();
+                edtemail.setError("Not correct format");
+                edtemail.requestFocus();
+            }else if(password.isEmpty()) {
+                Toast.makeText(NewAccountActivity.this, "Please " +
+                        "enter the password!", Toast.LENGTH_SHORT).show();
+                edtpassword.setError("This field cannot be empty");
+                edtpassword.requestFocus();
+            }else if(password.length()<6){
+                Toast.makeText(NewAccountActivity.this,"Password must " +
+                        "have at least 6 characters",Toast.LENGTH_SHORT).show();
+                edtpassword.setError("More than 6 characters");
+                edtpassword.requestFocus();
+            } else if(confirmPassword.isEmpty()){
+                edtconfirmPassword.setError("Please confirm the password");
+                edtconfirmPassword.requestFocus();
+            }else if(!password.equals(confirmPassword)){
+                edtconfirmPassword.setError("Passwords don't match");
+                edtconfirmPassword.requestFocus();
+            } else {
+
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+
+                if (!(username.isEmpty() && email.isEmpty()
+                        && password.isEmpty()
+                        && confirmPassword.isEmpty())) {
+
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration Successfully completed", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    });
+                }
+            }
+
+        });
     }
 }
