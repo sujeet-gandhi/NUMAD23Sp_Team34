27f6eefe7872bc42925af498fce937d2383b39f7
Gagana Ananda <gagana.131214@gmail.com>
2023-04-12 15:27:59 -0400

    Login and account Activity changes

--- app/src/main/java/com/neu/numad23sp_team_34/wanderlust/login/NewAccountActivity.java
+++ app/src/main/java/com/neu/numad23sp_team_34/wanderlust/login/NewAccountActivity.java
@@ -1,8 +1,13 @@
+//newaccount
+
 package com.neu.numad23sp_team_34.wanderlust.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.widget.Button;
@@ -10,12 +15,22 @@ import android.widget.EditText;
 
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import com.neu.numad23sp_team_34.R;
 import com.neu.numad23sp_team_34.wanderlust.User;
+import com.neu.numad23sp_team_34.wanderlust.home.HomeActivity;
 
 public class NewAccountActivity extends AppCompatActivity {
 
@@ -31,7 +46,7 @@ public class NewAccountActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
-    User user;
+    User users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,16 +102,35 @@ public class NewAccountActivity extends AppCompatActivity {
                 edtConfirmPassword.setError("Passwords don't match");
                 edtConfirmPassword.requestFocus();
             } else {
-                FirebaseAuth auth = FirebaseAuth.getInstance();
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
                 if (!(username.isEmpty() && email.isEmpty()
                         && password.isEmpty()
                         && confirmPassword.isEmpty())) {
-
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
+                            //Store the user details in firebase.
+                            String userid = task.getResult().getUser().getUid();
+                            users =new User(username,email,password);
+                            firebaseDatabase.getReference().child("WanderLustUser").child(userid).setValue(users);
+
                             Toast.makeText(getApplicationContext(), "Registration Successfully completed", Toast.LENGTH_SHORT).show();
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            //email verification.
+                            firebaseUser.sendEmailVerification();
+
+                            //Opening an activity after registration.
+                            Intent intent = new Intent(NewAccountActivity.this, HomeActivity.class);
+//
+                            //Once finish registering, clear out the activities behind.
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                            startActivity(intent);
+                            finish();
+                            //close register activity.
 
                         } else {
+
                             Toast.makeText(getApplicationContext(), task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                         }
 
