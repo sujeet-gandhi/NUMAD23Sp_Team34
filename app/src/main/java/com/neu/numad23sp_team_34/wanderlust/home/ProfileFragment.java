564cf9a66ae57ad7ca954742573ee6624bf23ea4
sujeetgandhi <sujeetgandhi014@gmail.com>
2023-04-16 18:49:58 -0400

    Add profile page

--- app/src/main/java/com/neu/numad23sp_team_34/wanderlust/home/ProfileFragment.java
+++ app/src/main/java/com/neu/numad23sp_team_34/wanderlust/home/ProfileFragment.java
@@ -1,66 +1,138 @@
 package com.neu.numad23sp_team_34.wanderlust.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.neu.numad23sp_team_34.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.neu.numad23sp_team_34.databinding.FragmentProfileBinding;
+import com.neu.numad23sp_team_34.project.Story;
+import com.neu.numad23sp_team_34.wanderlust.home.adapter.StoryAdapter;
+import com.neu.numad23sp_team_34.wanderlust.login.LoginActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private List<Story> myStories;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private StoryAdapter adapter;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
     }
 
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater);
+
+        binding.myTripsList.setLayoutManager(new LinearLayoutManager(getContext(),
+                LinearLayoutManager.HORIZONTAL, false));
+
+        myStories = new ArrayList<>();
+
+        adapter = new StoryAdapter(getContext(), myStories, true);
+
+        binding.myTripsList.setAdapter(adapter);
+
+
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            binding.email.setText(firebaseAuth.getCurrentUser().getEmail());
+            binding.username.setText(firebaseAuth.getCurrentUser().getDisplayName());
+
+            FirebaseDatabase.getInstance().getReference().child("stories")
+                    .addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            Story story = snapshot.getValue(Story.class);
+                            if (story != null && story.getUserName() != null && story.getUserName().equals(firebaseAuth.getCurrentUser().getDisplayName())) {
+                                myStories.add(story);
+                                adapter.notifyItemInserted(myStories.size() - 1);
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            Story changedStory = snapshot.getValue(Story.class);
+                            if (changedStory != null&& changedStory.getUserName() != null && changedStory.getUserName().equals(firebaseAuth.getCurrentUser().getDisplayName())) {
+                                for (int i = 0; i < myStories.size(); i++) {
+                                    if (changedStory.getId().equals(myStories.get(i).getId())) {
+                                        myStories.get(i).setDescription(changedStory.getDescription());
+                                        myStories.get(i).setImageUrl(changedStory.getImageUrl());
+                                        myStories.get(i).setTitle(changedStory.getTitle());
+                                        myStories.get(i).setKeywords(changedStory.getKeywords());
+                                        myStories.get(i).setItinerary(changedStory.getItinerary());
+                                        myStories.get(i).setReview(changedStory.getReview());
+                                        myStories.get(i).setRating(changedStory.getRating());
+
+                                        adapter.notifyItemChanged(i);
+                                    }
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                            Story story = snapshot.getValue(Story.class);
+                            int positionToBeRemoved = -1;
+                            if (story != null && story.getUserName() != null && story.getUserName().equals(firebaseAuth.getCurrentUser().getDisplayName())) {
+                                for (int i = 0; i < myStories.size(); i++) {
+                                    if (story.getId().equals(myStories.get(i).getId())) {
+                                        positionToBeRemoved = i;
+                                    }
+                                }
+
+                                if (positionToBeRemoved == -1) {
+                                    myStories.remove(story);
+                                    adapter.notifyDataSetChanged();
+                                } else {
+                                    myStories.remove(positionToBeRemoved);
+                                    adapter.notifyItemRemoved(positionToBeRemoved);
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding.logout.setOnClickListener(view -> {
+            firebaseAuth.signOut();
+            Intent intent = new Intent(getContext(), LoginActivity.class);
+            getContext().startActivity(intent);
+        });
+
+        return binding.getRoot();
+
     }
 }
